2ac7a9c9d82b7a467decc8c667794b0737e2ff98
NotDaloenFR <153146541+NotDaloenFR@users.noreply.github.com>
2026-04-20 23:16:09 +0800

    Fix formatting issues in OverdueAlerts component

--- src/pages/OverdueAlerts.js
+++ src/pages/OverdueAlerts.js
@@ -86,6 +86,7 @@ function OverdueAlerts({ appointments }) {
                     >
                       Delete
                     </button>
+                  </div>
               </div>
             ))}
           </div>
