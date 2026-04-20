1b215a0f4e0dbe3a45c1569afb3ac20bd3cb167f
rhey <rgamul@addu.edu.ph>
2026-04-21 01:56:16 +0800

    Add onDelete prop to OverdueAlerts component

--- src/pages/OverdueAlerts.js
+++ src/pages/OverdueAlerts.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-function OverdueAlerts({ appointments }) {
+function OverdueAlerts({ appointments, onDelete }) {
   const navigate = useNavigate();
   const [sortBy, setSortBy] = useState("daysOverdueDesc");
   // --- HERALD'S LOGIC: FILTERING & MATH ---
